d61a504f58b953cd9b4037e0ece8617ab0c15d31
NizeNize2K <nizeimbaboy.2@gmail.com>
2022-05-26 22:59:29 +0700

    deploy script

---
+++ deploy-node-starkware-by-nongnicelnwza007.sh
@@ -0,0 +1,33 @@
+echo ------------ Start Deploy starkware Node------------ 
+
+
+mkdir -p $HOME/deploy-startnet-node
+###Prepare env
+sudo apt-get update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+
+### install docker
+curl -fsSL get.docker.com -o get-docker.sh 
+sudo sh get-docker.sh
+
+### install docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+
+### SET URL
+
+read -p "Enter you alchemy url : " alchemyurl
+
+sed -i "s/SETURL/${alchemyurl}/g" .env
+
+cd $HOME/deploy-startnet-node
+
+### start docker-compose
+docker-compose down
+sleep 2
+docker-compose pull
+sleep 2
+docker-compose up -d
+
+echo Install Starknet Node Completed!!!!
